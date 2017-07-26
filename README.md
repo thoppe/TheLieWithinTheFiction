6ca5989af791a07bc160946ea2c5ac9d7ca409ee
Travis Hoppe <travis.hoppe@gmail.com>
2017-07-25 20:41:51 -0400

    Add encode option

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ http://thoppe.github.io/TheLieWithinTheFiction/demo.html
 - [x] Basic font remap (proof of concept)
 - [x] Demo webpage
 - [x] minify fonts
-- [ ] multi character
+- [ ] multi character (in progress)
+- [ ] fallback fonts
 - [ ] handle spaces (word boundaries)
--- multimap_code.py
+++ multimap_code.py
@@ -1,43 +1,82 @@
 import collections
+import bs4
 
 class translate_tables(object):
 
     def __init__(self):
         self.tables = collections.defaultdict(dict)
-        self.idx = -1
+        self.idx = 0
 
     def new_table(self):
         self.idx += 1
-        return self.tables[self.idx]
+        return self.tables[self.idx], self.idx
 
     def get_match(self, a1, a2):
         for k,tx in self.tables.items():
             if a1 not in tx:
-                return tx
+                return tx, k
             if tx[a1] == a2:
-                return tx
-        return None
+                return tx, k
+            
+        return None, None
 
     def __call__(self, a1, a2):
-        tx = self.get_match(a1, a2)
-        
+
+        # If there is no mapping return 0
+        if a1 == a2:
+            return 0
+
+        # Try to get a match
+        tx, key = self.get_match(a1, a2)
+
+        # If no match is made create a new table
         if tx is None:
-            tx = self.new_table()
+            tx, key = self.new_table()
             
         tx[a1] = a2
-        #print self.tables
+        return key
+
+    def encode(self, hidden_text, visible_text):
+
+        soup = bs4.BeautifulSoup('','lxml')
+        current_tag = None
+        current_idx = 0
+    
+        for a1,a2 in zip(visible_text, hidden_text):
+            key = self(a1,a2)           
+            print a1, a2, key
+
+            if key != current_idx:
+                # Create a new tag
+                if current_tag is not None:
+                    soup.append(current_tag)
+                    
+                current_tag = soup.new_tag("font{}".format(key))
+                current_idx = key
+
+            if current_tag.string is None:
+                current_tag.string = ''
+                
+            current_tag.string += a1
+
+        soup.append(current_tag)
+        return soup
+    
+
 
         
 
 if __name__ == "__main__":
-    hidden_text  = "The quick brown fox."
-    visible_text = "One lazy pizza day o"
+    #hidden_text  = "The quick brown fox."
+    #visible_text = "One lazy pizza day o"
     
     #hidden_text  = "abcabc"
     #visible_text = "xyzxxx"
-    
+
+    hidden_text  = "1234"
+    visible_text = "abcc"
+
     T = translate_tables()
-    for a1,a2 in zip(visible_text, hidden_text):
-        T(a1,a2)
-        
-    print T.tables
+    print T.encode(hidden_text, visible_text)
+
+
